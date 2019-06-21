71045c17c4970eaac2c8d8d6a184c677f0dfef0a
Robin Winslow <robin@robinwinslow.co.uk>
2019-06-21 13:21:45 +0100

    Improvements to FlaskBase:
    - Add redirects for /favicon.ico, /robots.txt, /humans.txt
    - Add errorhandlers for 404 and 500
    - Remove config.py, which only had 1 config item

--- canonicalwebteam/flask_base/app.py
+++ canonicalwebteam/flask_base/app.py
@@ -1,9 +1,13 @@
+# Standard library
+import os
+
+# Packages
 import flask
 import talisker.flask
-
 from werkzeug.contrib.fixers import ProxyFix
 from werkzeug.debug import DebuggedApplication
 
+# Local modules
 from canonicalwebteam.flask_base.context import base_context
 from canonicalwebteam.flask_base.converters import RegexConverter
 from canonicalwebteam.yaml_responses.flask_helpers import (
@@ -17,8 +21,11 @@ class FlaskBase(flask.Flask):
         self,
         name,
         service,
-        prepare_deleted=prepare_deleted(),
-        prepare_redirects=prepare_redirects(),
+        favicon_url=None,
+        template_404=None,
+        template_500=None,
+        robots_url=None,
+        humans_url=None,
         *args,
         **kwargs
     ):
@@ -26,23 +33,52 @@ class FlaskBase(flask.Flask):
 
         self.service = service
 
-        self.config.from_object(
-            "canonicalwebteam.flask_base.config.BaseConfig"
-        )
+        self.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "base_secret")
 
         self.url_map.strict_slashes = False
         self.url_map.converters["regex"] = RegexConverter
 
-        self.wsgi_app = (
-            ProxyFix(self.wsgi_app)
-            if not self.debug
-            else DebuggedApplication(self.wsgi_app)
-        )
+        self.wsgi_app = ProxyFix(self.wsgi_app)
 
-        self.before_request(prepare_redirects)
-        self.before_request(prepare_deleted)
+        if self.debug:
+            self.wsgi_app = DebuggedApplication(self.wsgi_app)
+
+        self.before_request(prepare_redirects())
+        self.before_request(prepare_deleted())
 
         self.context_processor(base_context)
 
         talisker.flask.register(self)
         talisker.logs.set_global_extra({"service": self.service})
+
+        # Default error handlers
+        if template_404:
+
+            @self.errorhandler(404)
+            def not_found_error(error):
+                return flask.render_template(template_404), 404
+
+        if template_500:
+
+            @self.errorhandler(500)
+            def internal_error(error):
+                return flask.render_template(template_500), 500
+
+        # Default routes
+        if favicon_url:
+
+            @self.route("/favicon.ico")
+            def favicon():
+                return flask.redirect(favicon_url)
+
+        if robots_url:
+
+            @self.route("/robots.txt")
+            def robots():
+                return flask.redirect(robots_url)
+
+        if humans_url:
+
+            @self.route("/humans.txt")
+            def humans():
+                return flask.redirect(humans_url)
