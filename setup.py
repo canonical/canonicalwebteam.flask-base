fbef41e883c1841518f045d0d7da497a3c945a23
Robin Winslow <robin@robinwinslow.co.uk>
2021-03-19 23:07:18 +0000

    v0.8.0: Update default caching headers

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.flask-base",
-    version="0.7.3",
+    version="0.8.0",
     description=(
         "Flask extension that applies common configurations"
         "to all of webteam's flask apps."
