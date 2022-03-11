3679bd733ba76adbe7aa3693fbea4eaea748be4d
Robin Winslow <robin@robinwinslow.co.uk>
2022-03-11 16:58:48 +0000

    v1.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.flask-base",
-    version="1.0.1",
+    version="1.0.2",
     description=(
         "Flask extension that applies common configurations"
         "to all of webteam's flask apps."
