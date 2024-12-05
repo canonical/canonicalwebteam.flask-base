ee8c49cc138b606623416fc012256faa752f7416
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2024-12-05 12:34:04 +0100

    Support Python 3.12

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 2.1.0 (2024-12-05)
+
+Add support for Python 3.12
+Update dependencies:
+
 # 2.0.0 (2024-07-04)
 
 Pin to Flask 2.3.3
@@ -51,67 +56,74 @@ Make them individually overrideable.
 # 0.7.2 (2021-01-07)
 
 ### Added
+
 Added the security header "X-Frame-Options" with the value "SAMEORIGIN"
 
 # 0.7.1 (2020-11-23)
 
 ### Added
+
 Changed `SEND_FILE_MAX_AGE_DEFAULT` back to the default value
 Set `Cache-Control: max-age 31536000` for requests with the v in the query string
 
 # 0.7.0 (2020-11-18)
 
 ### Added
+
 Set `SEND_FILE_MAX_AGE_DEFAULT` to a year (31536000)
 
 # 0.6.4 (2020-09-22)
 
 ### Added
+
 Pin gevent to version 20.6.2
 Pin greenlet to version 0.4.16
 
 # 0.6.3 (2020-05-27)
 
 ### Added
+
 Pin gevent to version 20.6.1
 
 # 0.6.2 (2020-06-11)
 
 ### Fix
-`versioned_static` shouldn't break the app if files are missing
 
+`versioned_static` shouldn't break the app if files are missing
 
 # 0.6.1 (2020-05-20)
 
 ### Features
+
 Serve favicon from static if it exists, otherwise fallback to favicon url.
 
 # 0.6.0 (2020-04-14)
 
 ### Added
-Update to talisker 0.18.0
 
+Update to talisker 0.18.0
 
 # 0.5.1 (2020-04-14)
 
 ### Added
-Pin gevent to version 1.4.0
 
+Pin gevent to version 1.4.0
 
 # 0.4.0 (2020-02-10)
 
 ### Added
-Set cache headers for all responses, overridable in the view.
 
+Set cache headers for all responses, overridable in the view.
 
 # 0.2.0 (2019-06-10)
 
 ### Features
+
 Added a context processor to FlaskBase instances containing common helpers needed acrosss
 diferent apps.
-
 
 # 0.1.0 (2019-06-05)
 
 ### Features
+
 Added FlaskBase class to wrap common functionality of canonical webteamm's flask applications
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.flask-base",
-    version="2.0.0",
+    version="2.1.0.dev0",
     description=(
         "Flask extension that applies common configurations"
         "to all of webteam's flask apps."
@@ -15,21 +15,23 @@ setup(
     author_email="webteam@canonical.com",
     url="https://github.com/canonical-web-and-design/canonicalwebteam.flask-base",
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.12",
     ],
+    python_requires=">=3.10",
     packages=find_packages(),
     install_requires=[
         "canonicalwebteam.yaml-responses[flask] (>=1,<2)",
-        "flask==3.1.0",
-        "jinja2 >= 3.1.2, < 3.2.0",
-        "gevent==24.11.1",
-        "greenlet==3.1.1",
-        "talisker[gunicorn,gevent,flask,prometheus,raven]",
-        "Werkzeug >=3.1.3, <3.2.0",
-        "markupsafe >=3, <4",
-        "itsdangerous >= 0.24, < 2.2.0",
+        "talisker[gunicorn,gevent,flask,prometheus,raven] >=0.21.4",
+        # Use latest version of Flask once Talisker supports werkzeug >=3
+        "flask==2.3.3",
+        "Werkzeug",
     ],
     dependency_links=[],
     include_package_data=True,
